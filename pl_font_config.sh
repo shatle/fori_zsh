74b6c29d2b1377efcab4d9c63a39b485fba57070
shatle <shatle@qq.com>
2013-07-06 11:21:36 +0800

    update pl_font_config.sh

--- pl_font_config.sh
+++ pl_font_config.sh
@@ -3,5 +3,6 @@
 mkdir ~/.fonts 
 cp ./PowerlineSymbols.otf ~/.fonts 
 mkdir ~/.config/fontconfig/conf.d 
-cp ./10-powerline-symbols.conf ~/.config/fontconfig/conf.d/ cp ./powerline-fonts/* ~/.fonts/
+cp ./10-powerline-symbols.conf ~/.config/fontconfig/conf.d/ 
+cp -rf ./powerline-fonts/* ~/.fonts/
 fc-cache -vf ~/.fonts
